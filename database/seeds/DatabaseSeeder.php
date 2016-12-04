328f8ae1b249a66f75d181db2a15430ba828fb43
Flaviu Porutiu [Glitch] <flaviu@glitch.ro>
2016-12-04 16:35:14 +0200

    Revert "Bodies refactoring"
    This reverts commit a5bca933b205413cc211d47c7b7d0475904df882.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,8 +21,7 @@ class DatabaseSeeder extends Seeder
     		'EmailTemplateSeeder',
     		'AddSuperAdmin',
             'AddRecrutementModuleSeeder',
-            'AddAnnouncementsRole',
-            'BodiesPatch'
+            'AddAnnouncementsRole'
     	);
 
     	$seeders = SeederLog::all();
