159dfb0069548bf11128222474ce12242c7cb465
i3anaan <i3anaan@live.nl>
2017-04-03 14:21:01 +0200

    Updated system roles

---
+++ database/seeds/AddSystemRoles.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Role;
+
+class AddSystemRoles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create([
+     			'name'			=>	'AEGEE-member',
+     			'code'			=>	'aegee',
+     			'system_role'	=>	1
+     		]);
+
+        $role = Role::create([
+     			'name'			=>	'Comité Directeur',
+     			'code'			=>	'comite_directeur',
+     			'system_role'	=>	1
+     		]);
+
+        $role = Role::create([
+     			'name'			=>	'Super Admin',
+     			'code'			=>	'super_admin',
+     			'system_role'	=>	1
+     		]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
     		'EmailTemplateSeeder',
     		'AddSuperAdmin',
             'AddRecrutementModuleSeeder',
-            'AddAnnouncementsRole'
+            'AddSystemRoles'
     	);
 
     	$seeders = SeederLog::all();
