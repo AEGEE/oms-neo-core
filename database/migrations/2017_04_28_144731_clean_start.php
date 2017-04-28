9cd6a70232a5f523251f46774629fcd9a2d56ac9
i3anaan <i3anaan@live.nl>
2017-04-28 19:08:03 +0200

    git doing stupid

---
+++ database/migrations/2017_04_28_144731_clean_start.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CleanStart extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      $tables = $this->getTables();
+      foreach($tables as $table) {
+        if (Schema::hasTable($table)) {
+          //Schema::rename($table, 'old_' . $table);
+          Schema::drop(DB::raw($table . " CASCADE"));
+        }
+      }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      $tables = $this->getTables();
+      foreach($tables as $table) {
+        if (Schema::hasTable('old_' . $table)) {
+          //Schema::rename('old_' . $table, $table);
+          Schema::dropIfExists($table);
+        }
+      }
+    }
+
+    private function getTables() {
+      return array(
+      'antennas',
+      'auths',
+      'board_members',
+      'bodies',
+      'body_memberships',
+      'countries',
+      'departments',
+      'email_templates',
+      'fee_users',
+      'fees',
+      'forums',
+      'global_options',
+      'menu_items',
+      'module_pages',
+      'modules',
+      'news',
+      'notifications',
+      'organization_roles',
+      'proxy_requests',
+      'recruted_comments',
+      'recruted_users',
+      'recrutement_campaigns',
+      'role_module_pages',
+      'roles',
+      'study_fields',
+      'study_types',
+      'types',
+      'user_roles',
+      'user_working_groups',
+      'users',
+      'working_groups',
+      'sessions',
+      'seeder_logs',
+      );
+    }
+}
