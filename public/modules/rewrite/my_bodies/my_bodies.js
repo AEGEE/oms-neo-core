e895de76aff1d120681ca5154939716bb4e0e800
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2017-05-05 12:49:04 +0300

    Drafted my bodies view and edit circles view

--- public/modules/rewrite/my_bodies/my_bodies.js
+++ public/modules/rewrite/my_bodies/my_bodies.js
@@ -6,8 +6,8 @@
         .module('app.my_bodies', [])
         .config(config)
         .directive('circlepanel', CircleDirective)
-
-        .controller('MyBodiesController', MyBodiesController);
+        .controller('MyBodiesController', MyBodiesController)
+        .controller('EditCirclesController', EditCirclesController);
 
     /** @ngInject */
     function config($stateProvider)
@@ -21,6 +21,16 @@
                     'pageContent@app': {
                         templateUrl: 'modules/rewrite/my_bodies/my_bodies.html',
                         controller: 'MyBodiesController as vm'
+                    }
+                }
+            })
+            .state('app.my_bodies.edit_circles', {
+                url: '/my_bodies/circle',
+                data: {'pageTitle': 'Edit Circles'},
+                views   : {
+                    'pageContent@app': {
+                        templateUrl: 'modules/rewrite/my_bodies/edit_circles.html',
+                        controller: 'EditCirclesController as vm'
                     }
                 }
             });
@@ -50,6 +60,52 @@
                 subtitle: "Nerds and stuff"
             }
         ];
+    }
+
+    function EditCirclesController() {
+        var vm = this;
+
+        vm.body = {
+            title: "AEGEE-Dresden",
+            subtitle: "The best local ever",
+            circles: [
+                {
+                    name: "Members"
+                },
+                {
+                    name: "Board"
+                }
+            ]
+        };
+
+        vm.users = [
+            {
+                name: "Nico",
+                circles: ["Members", "Board"]
+            },
+            {
+                name: "Dresdino",
+                circles: ["Members"]
+            }
+        ];
+
+        var intersectCircles = function(users, circles) {
+            var helper = function(usercircles, circles) {
+                return circles.map(function(item) {
+                    return {
+                        name: item.name,
+                        checked: (usercircles.findIndex((x) => x == item.name) !== -1)
+                    }
+                });
+            }
+
+            users = users.forEach(function(user, index, users) {
+                users[index].circles_fullarray = helper(user.circles, circles);
+            });
+        }
+
+        intersectCircles(vm.users, vm.body.circles);
+
     }
 
 })();
