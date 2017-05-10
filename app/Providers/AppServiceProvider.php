811bf995c39188211064d086ed3a6ba5c6e87350
i3anaan <i3anaan@live.nl>
2017-05-10 14:51:03 +0200

    Removed unused imports ni AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
