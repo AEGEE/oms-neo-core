528d76043e782108e24bcf118812fa64ea9447d9
i3anaan <i3anaan@live.nl>
2017-05-15 14:39:03 +0200

    Fixed controllers, added LoginMethodMiddleware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,45 +16,17 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 
-    protected function isOauthDefined() {
-    	$oAuth = config('oauth.oAuthProvider');
-        $oAuthDefined = false;
-        if(!empty($oAuth)) {
-            $oAuthDefined = true;
-        }
-
-        return $oAuthDefined;
-    }
-
-    protected function getOAuthProvider() {
-    	$oAuth = config('oauth.oAuthProvider');
-        return $oAuth;
-    }
-
-    protected function getDelegatedAdmin() {
-        return config('oauth.oAuthAdmin');
-    }
-
-    protected function getOAuthAllowedDomain() {
-    	$oAuthDomain = config('oauth.oAuthDomain');
-    	return $oAuthDomain;
-    }
-
-    protected function getOauthCredentials($adminId = '') {
-        if($this->getOAuthProvider() == 'google') {
-            $path = storage_path()."/app/".config('oauth.credentials').".json";
-            return json_decode(File::get($path), true);
-        } elseif($this->getOAuthProvider() == 'azure') {
-            $user = User::findOrFail($adminId);
-            $toReturn = array(
-                'token' =>  $user->oauth_token
-            );
-            return $toReturn;
-        }
-    }
-
     protected function getAppVersion() {
         $path = storage_path()."/../.version";
         return File::get($path);
+    }
+
+    protected function getUpdateArray($req, $fields, $allowNull = true) {
+        $arr = array();
+
+        foreach($fields as $field) {
+            if ($req->has($field) && ($allowNull || $req->get($field)!=null)) { $arr[$field] = $req->get($field);}
+        }
+        return $arr;
     }
 }
---
+++ app/Http/Middleware/LoginMethodMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User;
+
+class LoginMethodMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $loginMethod)
+    {
+        if ($loginMethod == 'oauth') {
+            if ($this->isOauthDefined()) {
+                return $next($request);
+            } else {
+                return response()->failure("Please use credentials as login method.");
+            }
+        } else {
+            return $next($request);
+        }
+    }
+
+    protected function isOauthDefined() {
+    	$oAuth = config('oauth.oAuthProvider');
+        $oAuthDefined = false;
+        if(!empty($oAuth)) {
+            $oAuthDefined = true;
+        }
+
+        return $oAuthDefined;
+    }
+
+    protected function getOAuthProvider() {
+    	$oAuth = config('oauth.oAuthProvider');
+        return $oAuth;
+    }
+
+    protected function getDelegatedAdmin() {
+        return config('oauth.oAuthAdmin');
+    }
+
+    protected function getOAuthAllowedDomain() {
+    	$oAuthDomain = config('oauth.oAuthDomain');
+    	return $oAuthDomain;
+    }
+}
