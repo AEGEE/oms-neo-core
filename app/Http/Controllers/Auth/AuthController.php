651e61533d7f9df122ac89a38090c10e850087d4
i3anaan <i3anaan@live.nl>
2017-05-15 14:11:56 +0200

    Added Laravel Auth files.

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Validator;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Registration & Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. By default, this controller uses
+    | a simple trait to add these behaviors. Why don't you explore it?
+    |
+    */
+
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'address_id'        => 'required|integer|exists:addresses,id',
+            'first_name'        => 'required|max:255',
+            'last_name'         => 'required|max:255',
+            'date_of_birth'     => 'required|date',
+            'email'             => 'required|email|max:255|unique:users,email',
+            'gender'            => 'required',
+            'phone'             => 'numeric',
+            'description'       => 'max:1024',
+            'password'          => 'required',
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        dd("hi");
+        parent::register($request);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        dump("creating user.");
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        //return $user;
+        return response()->success($user, null, 'User created');
+    }
+}
