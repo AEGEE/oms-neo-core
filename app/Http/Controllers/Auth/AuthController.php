ee17196ac15433352c327d027c780ee6e31de12c
i3anaan <i3anaan@live.nl>
2017-05-15 14:21:15 +0200

    Added CreateUserRequest, fixed email.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
             'first_name'        => 'required|max:255',
             'last_name'         => 'required|max:255',
             'date_of_birth'     => 'required|date',
-            'email'             => 'required|email|max:255|unique:users,email',
+            'contact_email'             => 'required|email|max:255|unique:users,email',
             'gender'            => 'required',
             'phone'             => 'numeric',
             'description'       => 'max:1024',
