4e706ff8930bd1381e81d826f67388ea108cb34b
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2017-06-09 12:26:12 +0300

    Implemented profile editing

--- public/modules/loggedIn/profile/profile.js
+++ public/modules/loggedIn/profile/profile.js
@@ -40,6 +40,9 @@
         // Data
         var vm = this;
         vm.user = {};
+        vm.permissions = {
+            edit_profile: true
+        };
         
         vm.getUser = function() {
             $http({
@@ -54,6 +57,67 @@
             }).catch(function(err) {showError(err);});
         }
         vm.getUser();
+
+        vm.getCountries = function() {
+            $http({
+                method: 'GET',
+                url: '/api/countries'
+            })
+            .then(function successCallback(response) {
+                vm.countries = response.data.data;
+            }).catch(function(err) {showError(err);});
+        }
+        vm.getCountries();
+
+        vm.changePicture = function() {
+            alert("Not implemented");
+        }
+
+        vm.showEditProfileModal = function() {
+            $('#editProfileModal').modal('show');
+        }
+
+        vm.saveProfile = function() {
+            // First submit the address, then the body
+            $http({
+                method: 'PUT',
+                url: '/api/addresses/' + vm.user.address.id,
+                data: vm.user.address
+            })
+            .then(function successCallback(response) {
+                // Create the body
+                $http({
+                    method: 'PUT',
+                    url: '/api/users/' + vm.user.id,
+                    data: vm.user
+                })
+                .then(function successCallback(response) {
+                    // Successfully saved that body
+                    $('#editProfileModal').modal('hide');
+                    $.gritter.add({
+                        title: 'Success',
+                        text: `Successfully edited profile`,
+                        sticky: false,
+                        time: 8000,
+                        class_name: 'my-sticky-class',
+                      });
+                    vm.getUser();
+                }).catch(function(err) {
+                    if(err.status == 422)
+                        vm.errors = err.data;
+                    else
+                        showError(err);
+                });
+
+            }).catch(function(err) {
+                if(err.status == 422)
+                    vm.errors = {
+                        address: err.data
+                    };
+                else
+                    showError(err);
+            });
+        }
     }
 
 
@@ -61,7 +125,11 @@
         // Data
         var vm = this;
         vm.user = {};
+        vm.permissions = {
+            edit_profile: false
+        };
         
+        // TODO check if own user, if yes display OwnProfileController
         vm.getUser = function() {
             $http({
                 method: 'GET',
