87f7f1b5998c0267e06fdd907fed530d3c272e67
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-08-24 14:40:38 +0200

    Added pagination for Collections
    This is inefficient though, no pagination is done on back-backend

--- app/Http/Middleware/PaginatorMiddleware.php
+++ app/Http/Middleware/PaginatorMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Input;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class PaginatorMiddleware {    /**
      * Handle an incoming request.
@@ -15,14 +16,36 @@ class PaginatorMiddleware {    /**
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        $size = Input::get('page_size', 10);
+
         $result = $next($request);
+
+        $page = Input::get('page', 1);
+        $per_page = Input::get('per_page', 10);
         $response = $result->getOriginalContent();
-        if (isset($response['data']) && is_a($response['data'], 'Illuminate\Database\Eloquent\Builder')) {
-            $response['data'] = $response['data']->paginate($size);
-            $response = json_encode($response);
-            $result->setContent($response);
+
+        if (isset($response['data'])) {
+            if (is_a($response['data'], 'Illuminate\Database\Eloquent\Builder')) {
+                $response['data'] = $response['data']->paginate($per_page);
+                $response = json_encode($response);
+                $result->setContent($response);
+            } else if(is_a($response['data'], 'Illuminate\Support\Collection')) {
+                $response['data'] = $this->paginateCollection($request, $response['data'], $per_page, $page);
+                $response = json_encode($response);
+                $result->setContent($response);
+            }
         }
         return $result;
+    }
+
+    private function paginateCollection($request, $collection, $per_page, $page) {
+        $offset = ($page * $per_page) - $per_page;
+
+        return new LengthAwarePaginator(
+            array_slice($collection->toArray(), $offset, $per_page, true), // Only grab the items we need
+            count($collection), // Total items
+            $per_page, // Items per page
+            $page, // Current page
+            ['path' => $request->url(), 'query' => $request->query()] // We need this so we can keep all old query parameters from the url
+        );
     }
 }
