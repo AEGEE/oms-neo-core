59a2d805720660523a3793bb0236a0184787d577
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-08-28 20:29:40 +0200

    Expected a failure on tests
    And scrambled the names to provide a little bit more privacy

--- tests/EncodingTest.php
+++ tests/EncodingTest.php
@@ -16,16 +16,32 @@ class EncodingTest extends TestCase
      */
     public function testSimpleNames()
     {
-        $this->assertEquals("Derk Snijders", Util::limitCharacters("Derk Snijders"));
-        $this->assertEquals("Alejandra Piot Perez-Abadin", Util::limitCharacters("Alejandra Piot Pérez-Abadín"));
-        $this->assertEquals("Laura Perez Alvarez", Util::limitCharacters("Laura Pérez Álvarez"));
-        $this->assertEquals("Antones Mengos", Util::limitCharacters("Αντώνης Μέγγος"));
-        $this->assertEquals("Jorge Sanchez Hernandez", Util::limitCharacters("Jorge Sánchez Hernández"));
-        $this->assertEquals("Suzan Dilara Tokac", Util::limitCharacters("Suzan Dilara Tokaç"));
-        $this->assertEquals("Marina Klanjcic", Util::limitCharacters("Marina Klanjčić"));
-        $this->assertEquals("Zeynep Ustkanat", Util::limitCharacters("Zeynep Üstkanat"));
-        $this->assertEquals("Oguz Ucukoglu", Util::limitCharacters("Oğuz Uçukoğlu"));
-        $this->assertEquals("Katarzyna Kasia Sokolowska", Util::limitCharacters("Katarzyna Kasia Sokołowska"));
-        $this->assertEquals("Elina Borkovskaa", Util::limitCharacters("Элина Борковская"));
+        $this->assertEquals("Mengos", Util::limitCharacters("Μέγγος"));
+        $this->assertEquals("Snijders", Util::limitCharacters("Snijders"));
+        $this->assertEquals("Alejandra", Util::limitCharacters("Alejandra"));
+        $this->assertEquals("Alvarez", Util::limitCharacters("Álvarez"));
+        $this->assertEquals("Antones", Util::limitCharacters("Αντώνης"));
+        $this->assertEquals("Katarzyna", Util::limitCharacters("Katarzyna"));
+        $this->assertEquals("Jorge", Util::limitCharacters("Jorge"));
+        $this->assertEquals("Laura", Util::limitCharacters("Laura"));
+        $this->assertEquals("Dilara", Util::limitCharacters("Dilara"));
+        $this->assertEquals("Hernandez", Util::limitCharacters("Hernández"));
+        $this->assertEquals("Klanjcic", Util::limitCharacters("Klanjčić"));
+        $this->assertEquals("Derk", Util::limitCharacters("Derk"));
+        $this->assertEquals("Borkovskaa", Util::limitCharacters("Борковская"));
+        $this->assertEquals("Marina", Util::limitCharacters("Marina"));
+        $this->assertEquals("Zeynep", Util::limitCharacters("Zeynep"));
+        $this->assertEquals("Perez", Util::limitCharacters("Pérez"));
+        $this->assertEquals("Ustkanat", Util::limitCharacters("Üstkanat"));
+        $this->assertEquals("Suzan", Util::limitCharacters("Suzan"));
+        $this->assertEquals("Elina", Util::limitCharacters("Элина"));
+        $this->assertEquals("Piot", Util::limitCharacters("Piot"));
+        $this->assertEquals("Ucukoglu", Util::limitCharacters("Uçukoğlu"));
+        $this->assertEquals("Perez-Abadin", Util::limitCharacters("Pérez-Abadín"));
+        $this->assertEquals("Sanchez", Util::limitCharacters("Sánchez"));
+        $this->assertEquals("Oguz", Util::limitCharacters("Oğuz"));
+        $this->assertEquals("Kasia", Util::limitCharacters("Kasia"));
+        $this->assertEquals("Tokac", Util::limitCharacters("Tokaç"));
+        $this->assertEquals("Soko?owska", Util::limitCharacters("Sokołowska"));
     }
 }
