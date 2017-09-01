4cd672489ded02500fb25da950bf30e83d3a225b
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-09-01 17:07:15 +0200

    Added dockerfile HEALTHCHECK

---
+++ app/Console/Commands/CheckHealth.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckHealth extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'service:health';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Checks the health of the service using the CLI';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (!\DatabaseSeeder::isSeeded()) {
+            $this->error("OMS-CORE Service is unhealthy");
+            exit(1);
+        } else {
+            $this->info("OMS-CORE Service is healthy");
+            exit(0);
+        }
+    }
+}
