c50901152e56c8cdaa2d3f15790df1d8b50b20db
i3anaan <i3anaan@users.noreply.github.com>
2017-10-04 23:38:53 +0200

    Silenced most of the bootstrap script (#67)

--- docker/omscore/bootstrap.sh
+++ docker/omscore/bootstrap.sh
@@ -16,28 +16,28 @@ else
     fi
 
 	cd /var/www
-    composer install --quiet   || { echo "Error at composer install"; exit 10; }
-	php artisan config:clear   || { echo "Error at config:clear"; exit 11; }
-	php artisan clear-compiled || { echo "Error at clear-compiled"; exit 12; }
-	php artisan config:cache   || { echo "Error at config:cache (1)"; exit 13; }
-	php artisan migrate        || { echo "Error at migrate"; exit 14; }
-	php artisan key:generate   || { echo "Error at key:generate"; exit 15; }
-	php artisan config:cache   || { echo "Error at config:cache (2)"; exit 16; }
-	php artisan db:seed        || { echo "Error at db:seed"; exit 17; }
-	php artisan config:cache   || { echo "Error at config:cache (3)"; exit 18; }
+    composer install --quiet       || { echo "Error at composer install"; exit 10; }
+	php artisan config:clear -q    || { echo "Error at config:clear"; exit 11; }
+	php artisan clear-compiled -q  || { echo "Error at clear-compiled"; exit 12; }
+	php artisan config:cache -q    || { echo "Error at config:cache (1)"; exit 13; }
+	php artisan migrate -q         || { echo "Error at migrate"; exit 14; }
+	php artisan key:generate -q    || { echo "Error at key:generate"; exit 15; }
+	php artisan config:cache -q    || { echo "Error at config:cache (2)"; exit 16; }
+	php artisan db:seed -q         || { echo "Error at db:seed"; exit 17; }
+	php artisan config:cache -q    || { echo "Error at config:cache (3)"; exit 18; }
 
 	# Make omscore write out the api-key
-  echo "Write out API Key:"
+    echo "Write out API Key:"
 	echo "app()->call([app()->make('App\\Http\\Controllers\\ModuleController'), 'getSharedSecret'], [])->original;" | php artisan tinker || { echo "Error at artisan tinker"; exit 17; }
 
-  cp /var/www/storage/key /var/shared/api-key
+    cp /var/www/storage/key /var/shared/api-key
 
 	# Copy the key into the volume mount so other
 	mkdir -p /var/shared
 	cp /var/www/storage/key /var/shared/api-key
 
 	npm install
-  gulp
+    gulp --silent &> /dev/null # Output is sent to null, disabling it
 
 	mkdir -p storage
 	mkdir -p /var/shared/strapstate
