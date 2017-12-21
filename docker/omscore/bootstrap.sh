cbb3b891a3f967df186064eaae128a2df9611833
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-12-21 22:58:53 +0100

    More lightweight omscore Removed unecessary language settings Removed some unnecessary env settings Set the bootstrap script to print its commands

--- docker/omscore/bootstrap.sh
+++ docker/omscore/bootstrap.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
-#better safe than sorry - I still have to read about permissions for volumes
-# in overlay-fs
+set -x #echo on
 
 if [ -f "/var/shared/strapstate/omscore" ]
 then
-	echo "[core bootstrap] Bootstrap-file found, not executing bootstrap script"
+	echo "[bootstrap] Bootstrap-file found, not executing bootstrap script"
 else
-	echo "[core bootstrap] Bootstrapping..."
+	echo "[bootstrap] Bootstrapping..."
     sleep 15 #give time to postgres container to startup
 
     if [ ! -f /var/www/.env ]; then
-      echo "[core bootstrap] No .env file found, copying from .env.example"
+      echo "[bootstrap] No .env file found, copying from .env.example"
       cp /var/www/.env.example /var/www/.env
     fi
 
@@ -27,14 +25,14 @@ else
 	php artisan config:cache -q    || { echo "[core bootstrap] Error at config:cache (3)"; exit 18; }
 
 	# Make omscore write out the api-key
-    echo "[core bootstrap] Write out API Key:"
+    echo "[bootstrap] Write out API Key:"
 	echo "app()->call([app()->make('App\\Http\\Controllers\\ModuleController'), 'getSharedSecret'], [])->original;" | php artisan tinker || { echo "[core bootstrap] Error at artisan tinker"; exit 17; }
 
 	# Copy the key into the shared volume mount so other containers can access it
 	mkdir -p /var/shared
 	cp /var/www/storage/key /var/shared/api-key
 
-  echo "[core bootstrap] installing npm packages"
+    echo "[bootstrap] installing npm packages"
 	npm install
     gulp --silent &> /dev/null # Output is sent to null, disabling it
 
@@ -44,5 +42,5 @@ else
 	# Create a file on strapstate to indicate we do not need to run this again
 	touch /var/shared/strapstate/omscore
 
-	echo "[core bootstrap] Bootstrap finished"
+	echo "[bootstrap] Bootstrap finished"
 fi
